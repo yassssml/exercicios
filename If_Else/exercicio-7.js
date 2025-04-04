8ec30895355191b36a0e61c236b01e64a23eddb6
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 11:05:26 -0300

    Utilizar estruturas condicionais + Utilizar operadores aritméticos

---
+++ If_Else/exercicio-7.js
@@ -0,0 +1,8 @@
+let salário = 3000
+
+if (salário > 2000 * 0.10) {
+    console.log('Voce recebeu 10% de desconto!')
+}
+if (salário <= 2000 * 0.05) {
+    console.log('Voce recebeu 5% de desconto!')
+}
