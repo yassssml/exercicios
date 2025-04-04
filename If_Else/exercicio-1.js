5351d04e5a96a03809f52623069dc38e540f19e2
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 10:30:07 -0300

    feat(if-else): adiciona verificação de positivo, negativo ou zero

---
+++ If_Else/exercicio-1.js
@@ -0,0 +1,10 @@
+let numb = 5
+ if (numb > 0) {
+    console.log('Esse numero é positivo')
+ }
+ if (numb < 0) {
+    console.log('Esse numero é negativo')
+ }
+ if (numb = 0) {
+    console.loog('Esse numero é zero')
+ }
