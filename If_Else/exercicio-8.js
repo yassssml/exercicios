3a16fa83fba16435f8e65ad77c373b518b104ef1
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 11:17:23 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ If_Else/exercicio-8.js
@@ -0,0 +1,12 @@
+let numb1 = 4
+let numb2 = 5
+let numb3 = 6
+
+if (numb1 < numb2 && numb1 < numb3)
+    console.log( numb1 + ' é menor de todos')
+
+else if (numb2 < numb1 && numb2 < numb3)
+    console.log(numb2 + ' é menor de todos')
+
+else if (numb3 < numb1 && numb3 < numb2)
+    console.log(numb3 +' é menor de todos')
