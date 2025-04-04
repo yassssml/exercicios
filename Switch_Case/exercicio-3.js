4e332dfda172188de2587ecf327a29cf3c7fbd79
yassssml <ycrisostomo051@gmail.com>
2025-04-04 15:52:59 -0300

    teste

---
+++ Switch_Case/exercicio-3.js
@@ -0,0 +1,15 @@
+let numb = 4
+
+switch (numb) {
+    case 1:
+        console.log(50+50)
+        break;
+    case 2:
+        console.log(50-50)
+        break;
+    case 3:
+        console.log(50*50)
+        break;
+    case 4:
+        console.log(50/50)
+}
