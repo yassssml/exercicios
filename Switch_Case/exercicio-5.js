a8edb11fff780f46cc736f6479b464d31ac8dcd9
yassssml <ycrisostomo051@gmail.com>
2025-04-04 16:05:52 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/exercicio-5.js
@@ -0,0 +1,16 @@
+let estação = 4
+switch(estação) {
+    case 1:
+        console.log('Verão');
+        break;
+
+    case 2:
+        console.log('Inverno');
+        break;
+    case 3:
+        console.log('Outono');
+        break;
+    case 4:
+        console.log('Primavera');
+        break;
+}
