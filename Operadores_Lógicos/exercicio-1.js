8cb6dfecb796e21a5696302fea76bea42fef4b77
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 08:09:25 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

---
+++ Operadores_Lógicos/exercicio-1.js
@@ -0,0 +1,12 @@
+let numb1 = 5
+let numb2 = -2
+
+if (numb1 > 0) {
+ console.log('Esse numero é positivo!')
+} else if (numb1 < 0) {
+    console.log('Esse numero é negativo')
+}if (numb2 > 0) {
+    console.log('Esse numero é positivo!')
+} else if (numb2 < 0) {
+    console.log('Esse numero é negativo')
+ }
