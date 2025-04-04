21aff39e23bec6cbae93f05d84b5e26e13c5686d
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 10:41:19 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/exercicio-3.js
@@ -0,0 +1,15 @@
+let idade = 12
+ if (idade >= 0 && idade <= 12) {
+    console.log('Criança')
+ }
+ if (idade >= 13 && idade <= 17) {
+    console.log('Adolescente')
+ }
+ if (idade >= 18 && idade <= 59) {
+    console.log('Adulto')
+ }
+ if (idade >= 60) {
+    console.log('Idoso')
+ } else {
+    console.log(idade)
+ }
