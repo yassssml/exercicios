ceb5e7aba986bc15f7b3d4453a03916ccb2e2062
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-04-04 10:35:39 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ If_Else/exercicio-2.js
@@ -0,0 +1,8 @@
+let numb1 = 20
+let numb2 = 5
+
+if (numb1 > numb2) {
+    console.log('O primeiro numero é maior que o segundo')
+}else {
+    console.log('O segundo numero é maior que o primeiro')
+}
