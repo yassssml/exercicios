86483cbd9627fc1170f9cad3c5d69a960bdeea16
yassssml <ycrisostomo051@gmail.com>
2025-04-04 16:03:13 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/exercicio-4.js
@@ -0,0 +1,18 @@
+let nota = 4
+switch(nota) {
+    case 1: nota >= 9 && nota <= 10
+      console.log('Nota A')
+      break;
+    case 2: nota >= 7 && nota <= 8
+      console.log('Nota B')
+      break;
+    case 3: nota >= 5 && nota <= 6
+      console.log('Nota C')
+      break;
+    case 4: nota >= 3 && nota <= 4
+      console.log('Nota D')
+      break;
+    case 5: nota >= 0 && nota <= 2
+      console.log('Nota E')
+      break;
+}
